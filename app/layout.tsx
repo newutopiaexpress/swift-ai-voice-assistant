2ce691113822331429ab6f1383f0215906d030ee
utopiaexpress123 <ricksanchezy2x@gmail.com>
2024-08-08 00:35:11 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 				className={clsx(
 					GeistSans.variable,
 					GeistMono.variable,
-					"py-8 px-6 lg:p-10 dark:text-white bg-stone-100 dark:bg-black min-h-dvh flex flex-col justify-between antialiased font-sans select-none"
+					"py-8 px-6 lg:p-10 dark:text-white bg-white dark:bg-black min-h-dvh flex flex-col justify-between antialiased font-sans select-none"
 				)}
 			>
 				<main className="flex flex-col items-center justify-center grow">
